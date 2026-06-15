443ff9123268ff1bddece7d278a744b8c0ee0a63
jasper-nyambichu <nyambichujasper@gmail.com>
2026-06-16 00:43:24 +0300

    add CRUD Operations

--- example/src/main/java/com/moseti/example/FirstController.java
+++ example/src/main/java/com/moseti/example/FirstController.java
@@ -1,22 +1,58 @@
 package com.moseti.example;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class FirstController {
 
- @GetMapping("/get")
-    public String sayHello(){
+ private final StudentRepository studentRepository;
 
-        return "This is my first get mapping request";
+    public FirstController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
 
-    @PostMapping("/post")
-    public String post(
-          @RequestBody  String message
+
+    @PostMapping("/students")
+    public Student post(
+          @RequestBody  Student student
+
     ){
-        return "Request accepted and the message is: "+message;
+
+       return studentRepository.save(student);
+
+    }
+
+    @GetMapping("/students")
+    public List<Student> findAllStudents() {
+
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/students/{student-id}")
+    public Student findStudentById(
+            @PathVariable("student-id") Integer id
+    ){
+        return studentRepository.findById(id)
+                .orElse( new Student());
+    }
+
+    @GetMapping("/students/search/{student-name}")
+    public List<Student> findStudentByName(
+            @PathVariable("student-name")  String name
+    ){
+        return studentRepository.findAllByFirstnameContaining(name);
+    }
+
+    @DeleteMapping("/students/{student-id}")
+    @ResponseStatus(HttpStatus.OK)
+        public void delete(
+            @PathVariable("student-id") Integer id
+    ){
+        studentRepository.deleteById(id);
     }
 
 
---
+++ example/src/main/java/com/moseti/example/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.moseti.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    List<Student> findAllByFirstnameContaining(String firstname);
+
+
+}
